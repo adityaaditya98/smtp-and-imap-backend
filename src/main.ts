160675b1082aa943275bc5d60cb92678801de131
Aditya B. <96489237+adityaaditya98@users.noreply.github.com>
2025-08-24 23:34:21 +0530

    small change

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+
+  app.enableCors({
+    origin: [
+      'http://localhost:4000', // React dev
+      'https://email-service-frontend-basic-pmkr5t5t8.vercel.app', // your deployed frontend
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // only if backend uses cookies/auth
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
